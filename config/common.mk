98ec1073f40a1a18ff80d419dbb1581f800ca393
lostystyg <andrew300399@gmail.com>
2023-04-25 17:21:09 +0400

    bastyon: changed version and product branc to bastyon

--- config/common.mk
+++ config/common.mk
@@ -3,7 +3,7 @@ $(call inherit-product-if-exists, vendor/bastyon/common.mk)
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= BastyonOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
